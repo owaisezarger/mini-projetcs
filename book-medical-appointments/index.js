e864ceee31058f01b6c8d1247064e830180aad25
owaisezarger <owaisezarger@gmail.com>
2023-05-25 22:38:20 +0530

    login and register form ui and conditional render

--- book-medical-appointments/index.js
+++ book-medical-appointments/index.js
@@ -4,3 +4,20 @@ document.addEventListener("DOMContentLoaded", () => {
   let navbarContainer = document.getElementById("navbar");
   navbarContainer.innerHTML = navbar;
 });
+
+let registerForm = document.getElementById("registerForm");
+registerForm.style.display = "none";
+
+let loginForm = document.getElementById("loginForm");
+let loginBtn = document.getElementById("loginBtn");
+let registerBtn = document.getElementById("registerBtn");
+
+loginBtn.addEventListener("click", () => {
+  loginForm.style.display = "block";
+  registerForm.style.display = "none";
+});
+
+registerBtn.addEventListener("click", () => {
+  loginForm.style.display = "none";
+  registerForm.style.display = "block";
+});
